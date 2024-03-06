f97420fa452e6637f8a33ea1cbdcb6bf660f1f26
chaitanya <54612528+Chaitanya-git-spec@users.noreply.github.com>
2024-03-06 16:27:17 +0530

    comit

--- mywebapp/obj/Release/net9.0/mywebapp.AssemblyInfo.cs
+++ mywebapp/obj/Release/net9.0/mywebapp.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("mywebapp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dec727c32daa804956c16c38072cf3d090b0e25")]
 [assembly: System.Reflection.AssemblyProductAttribute("mywebapp")]
